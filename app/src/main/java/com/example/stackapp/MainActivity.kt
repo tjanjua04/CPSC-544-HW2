0c7c6ee813dfffa11a76c30eb1e99d392a990ccd
Nathan Eduvala <nathaneduvala@yahoo.com>
2025-11-17 20:13:18 -0800

    Completed the stack and all the buttons work

--- app/src/main/java/com/example/stackapp/MainActivity.kt
+++ app/src/main/java/com/example/stackapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.stackapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, StackApp::class.java)
+            startActivity(intent)
         }
     }
 }
